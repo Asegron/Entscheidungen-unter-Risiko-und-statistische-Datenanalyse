1b4b207a95b5b89fa129ca10d5188146da115465
Asegron <58540933+Asegron@users.noreply.github.com>
2022-11-21 02:03:13 +0100

    Add files via upload

--- Auswerter.py
+++ Auswerter.py
@@ -1,8 +1,25 @@
 import csv
-import tkinter as tk
+from tkinter import *
 from tkinter import filedialog
 
-root = tk.Tk()
-root.withdraw()
+class Auswerter:
+     def __init__(self):
+        self.window = Tk()
+        self.button1 = Button(self.window,text = "Wählen Sie eine CSV-Datei zur Analyse aus!", 
+                            command=inputData)
+        self.button2 = Button(self.window,text = "Möchten Sie eine Häufigkeitstabelle oder eine Klassenhäufigkeitstabelle erstellen?")
+        self.button3 = Button(self.window,text = "Möchten Sie ein Balken- oder ein Tortendiagramm erstellen?")
+        self.button1.pack(pady=10)
+        self.button2.pack(pady=10)
+        self.button3.pack(pady=10)
+        self.c = Canvas(self.window,
+                            width=self.width(800),
+                            height=self.height(800))
+        self.c.pack()
+        self.window.mainloop()
 
-file_path = filedialog.askopenfilename()
+        #Ließt die CSV-Dateien ein
+        def inputData():
+            filedialog.askopenfilename()
+
+Auswerter()
