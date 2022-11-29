c65dc239061ccd4735cce96d3cd6e21da9fe1d51
Asegron <58540933+Asegron@users.noreply.github.com>
2022-11-29 07:00:14 +0100

    Update Auswerter.py

--- Auswerter.py
+++ Auswerter.py
@@ -6,20 +6,21 @@ class Auswerter:
      def __init__(self):
         self.window = Tk()
         self.button1 = Button(self.window,text = "Wählen Sie eine CSV-Datei zur Analyse aus!", 
-                            command=inputData)
-        self.button2 = Button(self.window,text = "Möchten Sie eine Häufigkeitstabelle oder eine Klassenhäufigkeitstabelle erstellen?")
-        self.button3 = Button(self.window,text = "Möchten Sie ein Balken- oder ein Tortendiagramm erstellen?")
-        self.button1.pack(pady=10)
-        self.button2.pack(pady=10)
-        self.button3.pack(pady=10)
+                            ).pack(pady=10)
+        self.button2 = Button(self.window,text = "Möchten Sie eine Häufigkeitstabelle oder eine Klassenhäufigkeitstabelle erstellen?").pack(pady=10)
+        self.button3 = Button(self.window,text = "Möchten Sie ein Balken- oder ein Tortendiagramm erstellen?").pack(pady=10)
         self.c = Canvas(self.window,
-                            width=self.width(800),
-                            height=self.height(800))
+                            width = 400,
+                            height = 200)
         self.c.pack()
         self.window.mainloop()
 
         #Ließt die CSV-Dateien ein
         def inputData():
             filedialog.askopenfilename(filetypes=("*.csv"))
+
+        def create_window(self):
+            global window
+            self.window = Tk.Toplevel(self)
 
 Auswerter()
