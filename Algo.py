b48bd4e39078326fe18d1a0f8ffcf6e80a88c8d5
Maximilian Mayer <93919078+mayersch@users.noreply.github.com>
2022-12-06 16:15:39 +0100

    Csv to list und erste Rechnungen

--- Algo.py
+++ Algo.py
@@ -1,34 +1,33 @@
 import csv
-with open('Motoren.csv')as daten:
-    reader = csv.reader(daten, delimiter= ';')
+import numpy as numpy
+import pandas as pandas
 
+
+def merkmal1():
     datencounter = 0
-    counter0 =0
-    mod0haufigkeit=0
-    counter1 =0
-    mod1haufigkeit=0
-
+    testlist = list()
     for row in reader:
-        print(row[0])
-        print(row[1])
-        if(row[0]==0  ):
-          counter0= counter1+1
 
-        if (row[0] == 1 ):
-            counter1 = counter1 + 1
+        if datencounter >= 1:
+            testlist.append(row[0])
 
+        datencounter = datencounter + 1
+    datencounter = datencounter - 1
+    print(datencounter)
+    print(testlist)
+    counter0 = testlist.count("0")
+    counter1 = testlist.count("1")
+    print(counter0)
+    print(counter1)
+    wahrscheinlichkeit0 = (100 / datencounter) * counter0
+    wahrscheinlichkeit1 = (100 / datencounter) * counter1
+    print(wahrscheinlichkeit0)
+    print(wahrscheinlichkeit1)
+    df = pandas.DataFrame(
 
+    )
 
+with open('Motoren.csv') as daten:
+    reader = csv.reader(daten, delimiter=';')
 
-        print('\t')
-        datencounter= datencounter+1
-
-    datencounter = datencounter-1
-    mod0haufigkeit= datencounter/counter0
-    mod1haufigkeit= datencounter/counter1
-    print('Anzahl der Daten:'+datencounter+ 'Haufigkeit Mod 0:'+mod0haufigkeit+'Häufigkeit Mod 1: '+mod1haufigkeit);
-
-
-
-
-
+    merkmal1()
