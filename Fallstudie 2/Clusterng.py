cd0044bc9a8adafe00430e054159f6a56c84236f
Asegron <daniel.stelz1@gmx.de>
2022-12-10 03:06:40 +0100

    yes

---
+++ Fallstudie 2/Clusterng.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pylab as pl
+from kneed import KneeLocator
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+import random
+import numpy as np
+
+deviation = np.random.uniform(1, 3)
+
+X, y_true = make_blobs(
+    n_samples=100,
+    centers=100,
+    cluster_std=deviation,
+    random_state=None
+)
+
+
+ranCluster = np.random.randint(1, 10)
+kmeans = KMeans(
+    init="random",
+    n_clusters=ranCluster,
+    n_init=10,
+    max_iter=300,
+    random_state=42
+)
+
+plt.scatter(X[:, 0], X[:, 1], s=50)
+plt.show()
