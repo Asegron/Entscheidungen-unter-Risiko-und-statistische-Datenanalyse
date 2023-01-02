35870c8275ced604b0794cfdc3f36467314de6d4
Maximilian Mayer <93919078+mayersch@users.noreply.github.com>
2023-01-02 14:01:43 +0100

    zwischenstand von blatt 2 teil a

--- Fallstudie 2/Clusterng.py
+++ Fallstudie 2/Clusterng.py
@@ -6,45 +6,88 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 
+def test():
+    # Zufällige Koordinaten generieren
+    x = [random.uniform(-100, 100) for _ in range(100)]
+    y = [random.uniform(-100, 100) for _ in range(100)]
+    # Diagramm erstellen
+    fig, ax = plt.subplots()
+    # Koordinaten plotten
+    ax.scatter(x, y)
+    # Diagramm anzeigen[
+    plt.show()
+
+cluster = []
+datenpunkte = []
+clusterx = []
+clustery = []
+
+
 def teil_a():
+
+
     #  zufällige wahl der Clusteranzahl
     k = random.randint(1, 10)
-    # Liste in der cluster
-    clusters = []
-    # Für jedes Cluster einen Ausgangspunkt generieren
-    for i in range(k):
-        ausgangspunkt = np.random.randint(-100, 100)
-        clusters.append(ausgangspunkt)
-    # Für jedes Cluster die zufälligen Datenpunkte generieren
-    data = []
-    for ausgangspunkt in clusters:
-        # generieren
-        # Samples are uniformly distributed over the half-open interval [low, high) (includes low, but excludes high).
-        # In other words, any value within the given interval is equally likely to be drawn by uniform.
-        sigma = random.uniform(1, 3)
 
-        # Unter beachtung der Standart abweichung werden die 100 datenounkte dem daten array hinzugefügt
-        point = np.random.normal(ausgangspunkt, sigma,
-                                 size=(100, 1))  # size gibt an wie viele daten punkte angelegt werden sollen
-        data.append(point)
-    # Alle Datenpunkte in einem Array zusammenführen
-    data = np.concatenate(data)
-    # plottest erst die ausgangspunkte und nach dem schließen die daten custer
-    # plt.plot(clusters, 'x')
-    # plt.show()
-    # plottet die cluster
-    plt.plot(data, 'x')
+    # hier werden die zufälligen punkte ci erzeugt
+    for i in range(k):
+        x = [random.uniform(-100, 100)]
+        y = [random.uniform(-100, 100)]
+        anfangswert = [x, y]
+        cluster.append(anfangswert)
+
+    clusterx = [point[0] for point in cluster ]
+    clustery = [point[1] for point in cluster]
+    plt.scatter(clusterx,clustery)
     plt.show()
+
+    for i in cluster:
+        datenpunkte_add(k)
+
+    datenx = [point[0] for point in datenpunkte]
+    dateny = [point[1] for point in datenpunkte]
+    plt.scatter(datenx,dateny)
+    plt.show()
+
+    print(datenpunkte)
+
+
+def datenpunkte_add(k):
+    print("hier")
+    sigma = random.uniform(1, 3)
+    ci = k
+    for i in range(100):
+        einerAusHundert = random.normalvariate(ci, sigma)
+        einerAusHundert2 = random.normalvariate(ci, sigma)
+        wert = [einerAusHundert, einerAusHundert2]
+        datenpunkte.append(wert)
 
 
 teil_a()
 
-print("Wie viele Cluster soll es im K-MEAN ALGO geben ? ")
-anzahlCluster = int(input("Anzahl:"))
 
+def teil_b():
+    print("Wie viele Cluster soll es im K-MEAN ALGO geben ? ")
+    anzahlCluster = int(input("Anzahl:"))
+    print("Wie viele Daten soll es im K-MEAN ALGO geben ? ")
+    anzahlDaten = int(input("Anzahl:"))
+    data = []
+    mittelwerte = []
 
+    for i in range(anzahlDaten):
+        zahl = random.randint(-30000, 30000)
+        zahl2 = random.randint(-30000, 30000)
+        y = [zahl, zahl2]
+        data.append(y)
 
+    plt.plot(data, "x")
+    plt.show()
+    plt.plot(mittelwerte, 'x')
+    plt.show()
 
+# test()
+
+# teil_b()
 #
 # deviation = np.random.uniform(1, 3)
 
