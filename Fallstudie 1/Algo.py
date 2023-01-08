c68724910be5f3e69b9700e9d1301f140943f963
Maximilian Mayer <93919078+mayersch@users.noreply.github.com>
2023-01-08 20:42:59 +0100

    KLassenhäufigkeiten tabelle fertig

--- Fallstudie 1/Algo.py
+++ Fallstudie 1/Algo.py
@@ -1,4 +1,6 @@
 import csv
+
+import numpy as np
 import pandas as pd
 from collections import defaultdict
 from collections import Counter
@@ -213,38 +215,29 @@ with open('Motoren.csv') as daten:
         daten_halter.sort()
         anzahl_klassen = int(round(math.sqrt(len(daten_halter)-1),0))
 
+        messgenauigkeit = 1
+        daten_halter = list(map(float, lst))
 
-        klassenbreite = ((daten_halter[len(daten_halter)-1]+ messgenauigkeit/2)-(daten_halter[0]- messgenauigkeit/2))/anzahl_klassen
-        print(klassenbreite)
+        daten_halter.sort()
+        anzahl_klassen = int(round(math.sqrt(len(daten_halter) - 1), 0))
 
-        datenklassen = []
-        x = 0
-        for i in range(anzahl_klassen-1):
-            datenklassen.append(daten_halter[x])
-            x = x+anzahl_klassen
-            print(datenklassen)
-        datenklassen.append(daten_halter[len(daten_halter)-1])
+        # Divide the data into 10 bins using the numpy histogram function
+        hist, edges = np.histogram(lst, bins=anzahl_klassen)
+        print(edges)
+        die_Behinderten_Klassen = []
+        print(hist)
 
+        # Print the frequency table
+        for i in range(len(hist)):
+            print("Bin", i + 1, ":", hist[i], "data points")
 
-        vorkomnisse = []
-        zähler = 0
-        print(datenklassen)
-        print(daten_halter)
+        table = [(left, right, count) for left, right, count in zip(edges[:-1], edges[1:], hist)]
 
-
-
-        for i in range(len(datenklassen)):
-            for x in range(len(daten_halter)):
-
-                if (daten_halter[x] >= datenklassen[i]):
-                    if (daten_halter[x] < datenklassen[i + 1]):
-                        zähler = zähler + 1
-
-
-            vorkomnisse.append(zähler)
-            zähler=0
-        df = pd.DataFrame({'Klassen': datenklassen, 'Häufigkeit': vorkomnisse})
+        # Erstelle eine Pandas-Datenframe aus der Liste
+        df = pd.DataFrame(table, columns=['Untere Schranke', 'Obere Schranke', 'Häufigkeit'])
+        # Anzeige der Tabelle
         return df
+
 
 
     def klassenhaufigkeitstabelle2(lst):
