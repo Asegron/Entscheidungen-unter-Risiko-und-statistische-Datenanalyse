210f0d7c5d04aa0693925e554c9c1e5b1ec6d58b
Asegron <daniel.stelz1@gmx.de>
2023-03-23 16:41:00 +0100

    markovketten

--- Fallstudie 3/wuerfel.py
+++ Fallstudie 3/wuerfel.py
@@ -1,12 +1,6 @@
 with open('wuerfel.txt', 'r') as file:
     text = file.read()
     array = text.split()
-    integer_array = []
-
-    for element in array:
-        try:
-            integer_array.append(int(element))
-        except ValueError:
-            pass
+    integer_array = [int(x) for x in array]
 
 print(integer_array)
