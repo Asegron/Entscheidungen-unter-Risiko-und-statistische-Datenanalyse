d754e83217a0e5323b0546bea87a642bfbef35d3
Asegron <58540933+Asegron@users.noreply.github.com>
2023-03-25 16:44:13 +0100

    Update and rename Fallstudie 3/versuch.py to viterbi.py

--- viterbi.py
+++ viterbi.py
@@ -1,4 +1,5 @@
 class Viterbi:
+    #initialisiert die Klasse mit den Parametern.
     def __init__(self, observations, states, start_p, trans_p, emit_p):
         self.observations = observations
         self.states = states
@@ -6,18 +7,20 @@ class Viterbi:
         self.trans_p = trans_p
         self.emit_p = emit_p
 
-    def run_viterbi_algorithm(self):
+    #Viterbi-Algorithmus
+    def viterbiAlgorithmus(self):
         V = [{}]
         for st in self.states:
             V[0][st] = {"prob": self.start_p[st] * self.emit_p[st][self.observations[0]], "prev": None}
 
+         # iteriert durch die Beobachtungen
         for t in range(1, len(self.observations)):
             V.append({})
             for st in self.states:
-                max_tr_prob = V[t - 1][self.states[0]]["prob"] * self.trans_p[self.states[0]][st]
+                max_tr_prob = V[t - 1][self.states[0]]["prob"] * self.trans_p[self.states[0]][st] #Multipliziert den aktuellen Zustand mit der Uebergangsmatrix
                 prev_st_selected = self.states[0]
                 for prev_st in self.states[1:]:
-                    tr_prob = V[t - 1][prev_st]["prob"] * self.trans_p[prev_st][st]
+                    tr_prob = V[t - 1][prev_st]["prob"] * self.trans_p[prev_st][st] #Multipliziert den vorherigen Zustand mit der Uebergangsmatrix
                     if tr_prob > max_tr_prob:
                         max_tr_prob = tr_prob
                         prev_st_selected = prev_st
@@ -38,13 +41,13 @@ class Viterbi:
         opt.append(best_st)
         previous = best_st
 
-
         for t in range(len(V) - 2, -1, -1):
             opt.insert(0, V[t + 1][previous]["prev"])
             previous = V[t + 1][previous]["prev"]
 
-        print ("The steps of states are " + " ".join(opt) + " with highest probability of %s" % max_prob)
+        print ("Die Schritte der Zustaende sind " + " ".join(opt) + " mit der hochsten Wahrscheinlichkeit von %s" % max_prob)
 
+    #Erstellt eine DP-Tabelle für die Pfade als visuelle Dastellung
     def dptable(self, V):
         yield " ".join(str(i).rjust(7) for i in range(len(V)))
         for state in V[0]:
@@ -54,8 +57,8 @@ with open('wuerfel.txt', 'r') as file:
     text = file.read()
     array = text.split()
     num_str = array[0]
-    #observations = [int(char) for char in list(array[0])]
-    observations = [1,1,1]
+    observations = [int(char) for char in list(array[0])]
+    #observations = [1,1,1]
     states = ["1", "2", "3", "4", "5", "6"]
 
     eingabe = input("Geben Sie 1 für fair oder 2 für gezinkt ein: ")
@@ -103,4 +106,4 @@ with open('wuerfel.txt', 'r') as file:
     }
 
 viterbi = Viterbi(observations, states, start_p, trans_p, emit_p)
-viterbi.run_viterbi_algorithm()
+viterbi.viterbiAlgorithmus()
